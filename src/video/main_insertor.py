890c4bbb884371395eb3caf7591519d5063cd120
irfansofyana <irfansofyana0305@gmail.com>
2020-10-04 14:34:49 +0700

    Finish Extractor

--- src/video/main_insertor.py
+++ src/video/main_insertor.py
@@ -2,7 +2,7 @@ from src.video.insertor import Inserter
 from src.helper.video_file import *
 
 if (__name__== "__main__" ):
-    path = 'sample/video/ex1.avi'
+    path = 'sample/video/ex3.avi'
     message_path = 'sample/text/short.txt'
     key = 'irfan'
 
@@ -13,19 +13,19 @@ if (__name__== "__main__" ):
         is_random_pixel = False
     )
 
-    output_path = 'sample/video/inserted.avi'
-    save_images_to_video(
-        output_path,
-        insert.directory_img,
-        inserted_frames,
-        insert.frame_rate
-    )
+    # output_path = 'sample/video/inserted.avi'
+    # save_images_to_video(
+    #     output_path,
+    #     insert.directory_img,
+    #     inserted_frames,
+    #     insert.frame_rate
+    # )
 
-    tes_vid = VideoFile(output_path)
-    tes_frames = tes_vid.frames
+    # tes_vid = VideoFile(output_path)
+    # tes_frames = tes_vid.frames
 
-    input_vid = VideoFile(path)
-    input_frames = input_vid.frames
+    # input_vid = VideoFile(path)
+    # input_frames = input_vid.frames
 
-    print((inserted_frames == tes_frames).all())
-    print((input_frames == tes_frames).all())
+    # print((inserted_frames == tes_frames).all())
+    # print((input_frames == tes_frames).all())
